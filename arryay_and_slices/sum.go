ecbf09dd1aa1cc276aae3caa408ab11230b11da9
singhprincejeet <singhprincejeet@gmail.com>
2024-07-12 18:06:04 -0400

    Arrays and Slices: SumAllTrails and Empty Sum test

--- arryay_and_slices/sum.go
+++ arryay_and_slices/sum.go
@@ -19,3 +19,17 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+
+	return sums
+}
